90f98ed23e7cb11240a9528db923d4578449f7e2
Lucas Lourenço <lucaspeixe2018@gmail.com>
2022-07-16 17:53:11 -0300

    trab

--- 13_07_2022_trab_bimestral/resources/views/components/datalist-disciplina.blade.php
+++ 13_07_2022_trab_bimestral/resources/views/components/datalist-disciplina.blade.php
@@ -40,7 +40,7 @@
                                 <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
                             </svg>
                         </a>
-                        <a href= "{{route('disciplina_professor.index')}}" class="btn btn-warning">
+                        <a href= "{{route('disciplina_professors.index')}}" class="btn btn-warning">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
